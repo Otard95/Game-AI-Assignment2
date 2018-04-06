766b2d406ad9c881f09d91345d4a03e97172d0c6
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-04-06 11:59:41 +0200

    added 'GameManager' with a test for the 'NavSuface' script

---
+++ A-star/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	NavSurface target;
+	Vector2 targetNode;
+
+	void Update () {
+
+		/**
+		 * # NavSurface Test
+		*/
+
+		if (Input.GetMouseButtonDown(0)) {
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				target = hit.transform.GetComponent<NavSurface>();
+				if (!target) return;
+
+				targetNode = target.ClosestNode(hit.point);
+			}
+
+		}
+
+	}
+
+	void OnDrawGizmos ()
+	{
+		
+		if (target) {
+			target.HightlightNode(targetNode);
+		}
+
+	}
+
+}
