3c75c03e0b7697c0a1fe5f6d89c9ad9b2241cde8
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-04-12 14:15:49 +0200

    Added 'PlayerController' to take use of the 'Pathfinder' and 'ActorMotor'

---
+++ A-star/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[RequireComponent(typeof(ActorMotor))]
+public class PlayerController : MonoBehaviour {
+
+	ActorMotor _motor;
+
+	[UsedImplicitly]
+	void Start () {
+		_motor = GetComponent<ActorMotor>();
+	}
+
+	[UsedImplicitly]
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+
+			NavSurface target;
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit)) {
+				target = hit.transform.GetComponent<NavSurface>();
+				if (!target) return;
+
+				PathFinder.Instance.GetPath(transform.position, hit.point, target, HandleReturnedPath);
+			}
+
+		}
+	}
+
+	void HandleReturnedPath (Vector3[] path) {
+		if (path == null) {
+			Debug.Log("Path not found.");
+			return;
+		}
+
+		_motor.FollowPath(path);
+	}
+
+}
