242ec466b0d640b69057cf9964d50c97ea0e77b3
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-04-17 11:31:02 +0200

    Added DragMove Tool and tool manager

---
+++ A-star/Assets/Scripts/DragMove.cs
@@ -0,0 +1,67 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class DragMove : MonoBehaviour {
+
+	[SerializeField] LayerMask movableLayers = 1;
+	[SerializeField] LayerMask placeableLayer = 512;
+
+	Vector3 _offset;
+	Transform _moving_transform;
+
+	[UsedImplicitly]
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1")) {
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit, int.MaxValue, movableLayers)) {
+
+				StartMoving(hit.transform);
+
+			}
+
+		} else if (Input.GetButtonUp("Fire1")) StopMoving();
+		else if (Input.GetButton("Fire1")) ContinueMoving();
+
+	}
+
+	void StartMoving (Transform _transform) {
+		
+		_moving_transform = _transform;
+		_offset = _transform.GetComponent<Collider>().bounds.extents;
+		_offset.x = 0;
+		_offset.z = 0;
+
+	}
+
+	void ContinueMoving () {
+
+		if (!_moving_transform) return;
+		_moving_transform.position = CursorWorldPos(placeableLayer) + _offset;
+
+	}
+
+	void StopMoving () {
+
+		if (!_moving_transform) return;
+		ContinueMoving();
+		_moving_transform = null;
+
+	}
+
+	Vector3 CursorWorldPos (LayerMask layer) {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit, int.MaxValue, layer)) {
+			return hit.point;
+		}
+
+		return Vector3.zero;
+
+	}
+
+}
---
+++ A-star/Assets/Scripts/ToolManager.cs
@@ -0,0 +1,81 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SetActive : UnityEvent<bool> {
+}
+
+[System.Serializable]
+public class ToolUISet {
+	public MonoBehaviour component;
+	public Button toolBtn;
+	public bool active;
+}
+
+[System.Serializable]
+public class ToggleBtn {
+	public bool active;
+	public Button button;
+	public Image btnImage;
+	public Sprite activeImage;
+	public Sprite inactiveImage;
+	public SetActive toggleEvent;
+}
+
+public class ToolManager : MonoBehaviour {
+
+	[SerializeField] Color activeColor = Color.green;
+	[SerializeField] Color inactiveColor = Color.gray;
+
+	[SerializeField] ToolUISet[] singleSelectTools;
+	[SerializeField] ToggleBtn[] toggleableTools;
+
+	[UsedImplicitly]
+	void Start () {
+
+		foreach (var tool in singleSelectTools) {
+			tool.toolBtn.GetComponent<Image>().color = tool.active ? activeColor : inactiveColor;
+			tool.component.enabled = tool.active;
+
+			tool.toolBtn.onClick.AddListener(() => {
+				DeactivateSingleSelectTools();
+				tool.active = true;
+				tool.toolBtn.GetComponent<Image>().color = tool.active ? activeColor : inactiveColor;
+				tool.component.enabled = tool.active;
+			});
+		}
+
+		foreach (var toggleBtn in toggleableTools) {
+
+			UpdateToggleButton(toggleBtn);
+
+			toggleBtn.button.onClick.AddListener(() => {
+				toggleBtn.active = !toggleBtn.active;
+				UpdateToggleButton(toggleBtn);
+			});
+
+		}
+
+	}
+
+	void UpdateToggleButton(ToggleBtn btn)
+	{
+		if (btn.btnImage) {
+			btn.btnImage.sprite = btn.active ? btn.activeImage : btn.inactiveImage;
+		}
+
+		btn.button.GetComponent<Image>().color = btn.active ? activeColor : inactiveColor;
+		if (btn.toggleEvent != null) btn.toggleEvent.Invoke(btn.active);
+	}
+
+	void DeactivateSingleSelectTools () {
+		foreach (var tool in singleSelectTools) {
+			tool.active = false;
+			tool.toolBtn.GetComponent<Image>().color = tool.active ? activeColor : inactiveColor;
+			tool.component.enabled = tool.active;
+		}
+	}
+
+}
